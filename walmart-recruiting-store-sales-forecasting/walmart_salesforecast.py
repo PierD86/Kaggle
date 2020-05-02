b1b4c5cd0f63ef2c66a44a9d606470a56888a45e
Piero Danti <piero_danti@yanmar.com>
2020-05-02 16:15:53 +0200

    add distplot for many transformation functions

--- walmart-recruiting-store-sales-forecasting/walmart_salesforecast.py
+++ walmart-recruiting-store-sales-forecasting/walmart_salesforecast.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import scipy.stats as st
+import datetime
 
 #import all input csv
 !dir
@@ -28,7 +29,10 @@ df.isnull().sum()
 #df.fillna(0,inplace=True)
 df.dropna(inplace=True)
 
+df['Date'] = pd.to_datetime(df['Date']) 
+
 #EDA
+############### CONTINUOS VARS #########################
 #this is how continuos variables look like
 fig, ax = plt.subplots(4,3, figsize=(16,16))
 sns.distplot(df['Weekly_Sales'], ax = ax[0,0], fit=st.norm)
@@ -44,7 +48,7 @@ sns.distplot(df['CPI'], ax = ax[3,0], fit=st.norm)
 sns.distplot(df['Unemployment'], ax = ax[3,1], fit=st.norm)
 
 
-#unkewing data:
+#unskewing data:
 #let's check if normalization helps to unskew data
 from sklearn.preprocessing import MinMaxScaler
 mms = MinMaxScaler()
@@ -72,6 +76,14 @@ def original(x):
 def power_2(x):
   return x**2  
 
+def yeojohnson(x):
+  try:
+    for var in x.columns:
+      x[var], _  = st.yeojohnson(x[var])
+  except:
+    x,_ = st.yeojohnson(x)
+  return x 
+
 dz = {}
 dz['sigmoid'] = [sigmoid, 'r']
 dz['tan'] = [np.tan, 'b']
@@ -79,8 +91,9 @@ dz['tanh'] = [np.tanh, 'm']
 dz['sin'] = [np.sin, 'y']
 dz['log'] = [np.log1p, 'k']
 dz['sqrt'] = [np.sqrt, 'g']
-dz['sqrt'] = [original, 'c']
+dz['original'] = [original, 'c']
 dz['power2'] = [power_2, 'lime']
+dz['yeojohnson'] = [yeojohnson, 'violet']
 
 
 def unskewner(dfx, dz_func, func_name, vars):
@@ -99,16 +112,42 @@ def unskewner(dfx, dz_func, func_name, vars):
     plt.suptitle(f'{func_name}')
     plt.savefig(f'dist_{func_name}.jpg')
     dz_mu, dz_sgm, dz_skw, dz_krt = {}, {}, {}, {}
-    for i in cont_var:
+    for i in vars:
         dz_mu[i] = np.mean(dz_func[func_name][0](dfx[i]))
         dz_sgm[i] = np.std(dz_func[func_name][0](dfx[i]))
         dz_skw[i] = st.skew(dz_func[func_name][0](dfx[i]))
         dz_krt[i] = st.kurtosis(dz_func[func_name][0](dfx[i]))
     return dz_mu, dz_sgm, dz_skw, dz_krt
 
-rslt_mu = {}
-rslt_sgm = {}
-rslt_skw = {}
-rslt_krt = {}
+#create distplot for each transformation in dz
+rslt_mu, rslt_sgm, rslt_skw, rslt_krt = {}, {}, {}, {}
 for k in dz:
-    rslt_mu[k],rslt_sgm[k],rslt_skw[k],rslt_krt[k] = unskewner(df_s,func_dict,k,cont_var)
+    rslt_mu[k],rslt_sgm[k],rslt_skw[k],rslt_krt[k] = unskewner(df_s,dz,k,cont_var)
+
+def heatmapper(dfx, dz_func, func_name, vars):
+    plt.figure(figsize=(10,10)) 
+    sns.heatmap(dz_func[func_name][0](dfx[vars]).corr(), annot = True, fmt='.2f', cmap ='Blues')
+    plt.suptitle(func_name)
+    plt.savefig(f'heatmap_{func_name}.jpg')
+
+#create heatmaps for each transformation in dz
+for k in dz:
+  heatmapper(df_s,dz,k,cont_var)
+
+
+#from all plots and analysing skewness and kurtosis of all transformations
+#seems very usefull to use the 'yeojohnson' methodology
+#heatmaps shows that aweak  linear correlation with Weekly_Sales is present 
+#only for 'Size' and 'MarkDown5'
+
+df_yeo = df_s.copy()
+yeo_lambda = {}
+for var in cont_var:
+  df_yeo[var],yeo_lambda[var]  = st.yeojohnson(df_s[var])
+
+############### CATEGORIVAL VARS #########################
+plt.figure(figsize=(10,10))
+sns.pairplot(df, vars=cont_var)
+plt.savefig(f'pairplot.jpg')
+
+df.head()
